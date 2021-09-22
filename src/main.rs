845c3ce06f0b175b0053d22261c5c63de5545e98
Yu-Ren Zhang <e8035669@gmail.com>
2021-09-22 23:15:45 +0800

    make a ping pong bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+    framework::standard::{
+        StandardFramework,
+    },
+    utils::MessageBuilder,
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "$ping" {
+
+            let response = MessageBuilder::new()
+                .push("Hello ")
+                .mention(&msg.author)
+                .build();
+
+            if let Err(why) = msg.channel_id.say(&ctx.http, response).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~"));
+
+    let token = env::var("DC_TOKEN").expect("Expected a token in the environment");
+
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
