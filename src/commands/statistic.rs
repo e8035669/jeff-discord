edb5be01047d834843fc2a5bd1c1541eac1a36cd
Yu-Ren Zhang <e8035669@gmail.com>
2022-12-17 09:02:21 +0800

    update emoji statistic

--- src/commands/statistic.rs
+++ src/commands/statistic.rs
@@ -10,19 +10,21 @@ use std::collections::HashMap;
 use std::time::Duration as StdDuration;
 use tracing::debug;
 
+use serenity::builder::CreateActionRow;
+use serenity::builder::CreateEmbed;
 use serenity::futures::StreamExt;
 use serenity::model::application::interaction::InteractionResponseType;
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
-use crate::commands::utils::argsort;
+// use crate::commands::utils::argsort;
 
 /// 統計某些東西
 #[group]
 #[commands(emojistat)]
 struct Statistic;
 
-/// 統計近30天的emoji使用
+/// 統計近180天的emoji使用
 #[command]
 #[owners_only]
 async fn emojistat(_ctx: &Context, _msg: &Message) -> CommandResult {
@@ -36,7 +38,7 @@ async fn emojistat(_ctx: &Context, _msg: &Message) -> CommandResult {
     }
 
     let current_time = Utc::now();
-    let old_time = current_time - Duration::seconds(60);
+    let old_time = current_time - Duration::days(180);
 
     let channels = &_guild.channels;
 
@@ -44,7 +46,9 @@ async fn emojistat(_ctx: &Context, _msg: &Message) -> CommandResult {
         if let Channel::Guild(ch) = ch {
             let msgs = query_messages(&_ctx, &ch, old_time.into()).await?;
 
-            let msgs2: Vec<_> = msgs.into_values().map(|m| m.content).collect();
+            let msgs = msgs.into_values().collect::<Vec<_>>();
+
+            let msgs2: Vec<_> = msgs.iter().map(|m| m.content.to_owned()).collect();
 
             lazy_static! {
                 static ref RE: Regex = Regex::new(r"(<a?)?:\w+:(\d{18}>)?").unwrap();
@@ -59,10 +63,29 @@ async fn emojistat(_ctx: &Context, _msg: &Message) -> CommandResult {
 
             emojis.iter().for_each(|e| *counts.get_mut(e).unwrap() += 1);
 
-            for (k, v) in counts.iter() {
-                let e = guild_emoji.get(k).unwrap();
-                debug!("{}: {}", e.name, v);
-            }
+            let react_emojis: Vec<(EmojiId, u32)> = msgs
+                .iter()
+                .flat_map(|m| m.reactions.clone())
+                .filter_map(|r| {
+                    if let ReactionType::Custom {
+                        animated: _,
+                        id,
+                        name: _,
+                    } = r.reaction_type
+                    {
+                        Some((id, r.count as u32))
+                    } else {
+                        None
+                    }
+                })
+                .filter(|r| guild_emoji.contains_key(&r.0))
+                .collect();
+
+            react_emojis
+                .iter()
+                .for_each(|r| *counts.get_mut(&r.0).unwrap() += r.1);
+
+            debug!("Collect {} messages", msgs.len());
         }
     }
 
@@ -75,35 +98,50 @@ async fn emojistat(_ctx: &Context, _msg: &Message) -> CommandResult {
         messages.push(format!("{}: {}", e2, c));
     }
 
-    let mut current_page = 0;
+    let sorted_count_chunks = counts.chunks(15).collect::<Vec<_>>();
+
+    let mut current_page: isize = 0;
 
     let mut m = _msg
         .channel_id
         .send_message(&_ctx, |m| {
-            m.content(&messages[current_page]).components(|c| {
-                c.create_action_row(|row| row.create_button(|b| b.label("Next").custom_id("1")))
-            })
+            m.set_embed(get_embed_message(
+                &guild_emoji,
+                &sorted_count_chunks,
+                current_page,
+            ))
+            .components(|c| c.add_action_row(create_buttom_row()))
+            .content(format!("{}", _msg.author.mention()))
         })
         .await?;
 
     let mut interaction_stream = m
         .await_component_interactions(&_ctx)
-        .timeout(StdDuration::from_secs(60 * 3))
+        .timeout(StdDuration::from_secs(60 * 10))
         .build();
 
     while let Some(interaction) = interaction_stream.next().await {
-        match interaction.data.custom_id.as_str() {
-            "1" => {
-                current_page = (current_page + 1) % messages.len();
+        let custom_id = interaction.data.custom_id.as_str();
+        match custom_id {
+            "prev" | "next" => {
+                let shift: isize = match custom_id {
+                    "prev" => -1,
+                    "next" => 1,
+                    _ => 0,
+                };
+
+                current_page =
+                    (current_page + shift).clamp(0, (sorted_count_chunks.len() - 1) as isize);
                 interaction
                     .create_interaction_response(&_ctx, |r| {
                         r.kind(InteractionResponseType::UpdateMessage)
                             .interaction_response_data(|m| {
-                                m.content(&messages[current_page]).components(|c| {
-                                    c.create_action_row(|row| {
-                                        row.create_button(|b| b.label("Next").custom_id("1"))
-                                    })
-                                })
+                                m.set_embed(get_embed_message(
+                                    &guild_emoji,
+                                    &sorted_count_chunks,
+                                    current_page,
+                                ))
+                                .components(|c| c.add_action_row(create_buttom_row()))
                             })
                     })
                     .await?;
@@ -112,7 +150,45 @@ async fn emojistat(_ctx: &Context, _msg: &Message) -> CommandResult {
         }
     }
 
+    m.edit(&_ctx, |m| m.components(|c| c)).await?;
+
     Ok(())
+}
+
+fn get_embed_message(
+    emojis: &HashMap<EmojiId, Emoji>,
+    all_emoji_counts: &Vec<&[(EmojiId, u32)]>,
+    page: isize,
+) -> CreateEmbed {
+    let mut embed = CreateEmbed::default();
+    embed.title("Emoji Statistic Result");
+    embed.description(get_message(emojis, all_emoji_counts, page));
+    embed.footer(|f| f.text(format!("Page {}/{}", page + 1, all_emoji_counts.len())));
+
+    embed
+}
+
+fn get_message(
+    emojis: &HashMap<EmojiId, Emoji>,
+    all_emoji_counts: &Vec<&[(EmojiId, u32)]>,
+    page: isize,
+) -> String {
+    let emoji_counts = all_emoji_counts[page as usize];
+    let ss = emoji_counts
+        .iter()
+        .map(|item| {
+            let e = emojis.get(&item.0).unwrap();
+            format!("{}: {}", e, item.1)
+        })
+        .collect::<Vec<String>>();
+    ss.join("\n")
+}
+
+fn create_buttom_row() -> CreateActionRow {
+    let mut r = CreateActionRow::default();
+    r.create_button(|b| b.label("Prev").custom_id("prev"))
+        .create_button(|b| b.label("Next").custom_id("next"));
+    r
 }
 
 async fn query_messages(
